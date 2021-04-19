22103250da591ffa93c42e79d9a6dc4659eed6eb
desainr <eniiseekay@gmail.com>
2021-04-19 11:51:05 -0400

    Added some error handling

--- draft_filter/index.js
+++ draft_filter/index.js
@@ -5,13 +5,31 @@ module.exports = async function (context, req) {
     context.log('Executing query request...');
 
     let result = [];
-    if (req.method === 'GET') {
-        const query = buildFilter(req.query);
+    let status = 200;
 
-        result = await executeQuery(query);
+    if (req.method === 'GET') {
+        let query = '';
+
+        try {
+            query = buildFilter(req.query);
+        } catch (ex) {
+            context.log({msg: 'Error occurred while parsing query parameters', ex, query: req.query});
+            status = 400;
+        }
+
+        if (query) {
+            try {
+                result = await executeQuery(query);
+            } catch (ex) {
+                context.log({msg: 'An error occured while processing SQL query', ex, query});
+                status = 500;
+            }
+        }
+
     }
 
     context.res = {
+        status,
         // status: 200, /* Defaults to 200 */
         body: result
     };
